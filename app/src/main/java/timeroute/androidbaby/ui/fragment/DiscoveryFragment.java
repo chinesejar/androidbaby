58041e309174aa716cb31ee5f43a5620236e2c86
chinesejar <chinesejar@outlook.com>
2017-08-02 12:22:58 +0800

    fixed the layout constaint in discovery fragment

--- app/src/main/java/timeroute/androidbaby/ui/fragment/DiscoveryFragment.java
+++ app/src/main/java/timeroute/androidbaby/ui/fragment/DiscoveryFragment.java
@@ -3,8 +3,8 @@ package timeroute.androidbaby.ui.fragment;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.design.widget.BottomNavigationView;
 import android.support.v4.app.Fragment;
-import android.support.v4.view.ViewPager;
 import android.support.v7.widget.LinearLayoutManager;
 import android.util.Log;
 import android.view.View;
